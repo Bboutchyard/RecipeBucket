c4997c9631c11f326454d8efefa2bcf15c88b50a
Brandon Boutchyard <brandonboutchyard@gmail.com>
2023-10-29 13:08:00 -0400

    update 10/29/2023

--- app/src/main/java/com/example/recipebucket/database.java
+++ app/src/main/java/com/example/recipebucket/database.java
@@ -1,6 +1,34 @@
 package com.example.recipebucket;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class database {
-    //Here we will add the database calls for all of the classes(names, ingredients, description,
-    //and images
+    //Here we will add the database calls for all of the classes(names, ingredients, description)
+
+    public void getRecipeName() {
+        //To do
+    }
+    public void setRecipeName() {
+        //To do
+    }
+    public void getIngredients() {
+        //To do
+    }
+    public void setIngredients() {
+        //To do
+    }
+    public void getMeasurement() {
+        //To do
+    }
+    public void setMeasurement() {
+        //To do
+    }
+    public void getInstructions() {
+        //To do
+    }
+    public void setInstructions() {
+        //To do
+    }
 }
--- app/src/main/java/com/example/recipebucket/recipeAdapter.java
+++ app/src/main/java/com/example/recipebucket/recipeAdapter.java
@@ -1,5 +1,44 @@
 package com.example.recipebucket;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class recipeAdapter {
     //Integrate database calls to ID's associated with recipes.java and addEditDeleteRecipe.java
+
+        private FirebaseFirestore db;
+
+        public recipeAdapter() {
+            db = FirebaseFirestore.getInstance();
+        }
+
+    public void addRecipeToFirestore(String recipeName, int servings, List<Map<String, Object>> ingredients, List<String> instructions) {
+        Map<String, Object> recipeData = new HashMap<>();
+        recipeData.put("recipeName", recipeName);
+        recipeData.put("servings", servings);
+        recipeData.put("ingredients", ingredients);
+        recipeData.put("instructions", instructions);
+
+        db.collection("recipes")
+                .add(recipeData)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // Recipe added successfully
+                        // Handle success as needed
+                    } else {
+                        // Handle the error
+                    }
+                });
+    }
+    }
+
+
+
+
+
 }
